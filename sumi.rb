a756da2ff54b4d17ffcbbea625cd3aca918d94f7
hsak <sakurai777@gmail.com>
2010-05-31 14:00:54 +0900

    構文解析追加

--- sumi.rb
+++ sumi.rb
@@ -1,3 +1,22 @@
-src = "1 + 2 * 3"
-tokens = src.split(/[\s]*([0-9]+|[+\-\/*])/).reject!{|x| x==""}
-p tokens
+class Parser 
+  def initialize
+    @infixs = {"*"=>20,"+"=>10}
+  end
+  def parse(src)
+    @tokens = src.split(/[\s]*([0-9]+|[+\-\/*])/).reject!{|x| x==""}
+    exp(0)
+  end
+  def exp(p)
+    t = @tokens.shift
+    while  (p1 = @infixs[@tokens[0]]) != nil
+      if p1 <= p
+        break
+      end
+      op = @tokens.shift
+      t = [t, op, exp(p1)]
+    end
+    t
+  end
+end
+
+p Parser.new.parse("1  +2*3*4+5+6*7")
